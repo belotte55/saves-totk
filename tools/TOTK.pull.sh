076ab5490c6ea75bd383cb2cdef3fa98982c390d
deck <belotte1355@gmail.com>
2025-05-17 15:49:26 +0200

    2025-05-17 15:49

---
+++ tools/TOTK.pull.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cd /home/deck/.local/share/yuzu/nand/user/save/0000000000000000/50D91EF7B1CB527939B060D1EFC1805F/0100F2C0115B6000
+
+git pull
---
+++ tools/TOTK.push.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cd /home/deck/.local/share/yuzu/nand/user/save/0000000000000000/50D91EF7B1CB527939B060D1EFC1805F/0100F2C0115B6000
+
+git add .
+git commit -m "$(date '+%Y-%m-%d %H:%M')"
+git push
